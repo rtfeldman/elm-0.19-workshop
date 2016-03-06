0edc438117e99c79692a14e5815ba06283c2ae97
Richard Feldman <richard.t.feldman@gmail.com>
2016-03-06 00:01:13 -0800

    Centralize npm installation in root, so you only have to do it once

---
+++ install.js
@@ -0,0 +1,10 @@
+var fs = require("fs");
+var execSync = require("child_process").execSync;
+var path = require("path");
+
+fs.readdirSync("stages").forEach(function(stage) {
+  var cwd = path.join(__dirname, "stages", stage);
+
+  console.log("Installing packages for " + cwd);
+  execSync("elm-package install --yes", { cwd: cwd });
+});
