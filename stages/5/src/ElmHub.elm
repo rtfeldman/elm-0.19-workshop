fe473e10410bd7d4793899b80384afc98a5c28ac
Richard Feldman <richard.t.feldman@gmail.com>
2016-03-06 00:28:00 -0800

    Remove elm.js and split out Main.elm

--- stages/5/src/ElmHub.elm
+++ stages/5/src/ElmHub.elm
@@ -12,26 +12,6 @@ import Json.Encode
 import Signal exposing (Address)
 
 
-main : Signal Html
-main =
-  app.html
-
-
-app : StartApp.App Model
-app =
-  StartApp.start
-    { view = view
-    , update = update
-    , init = ( initialModel, Effects.task (searchFeed initialModel.query) )
-    , inputs = []
-    }
-
-
-port tasks : Signal (Task Effects.Never ())
-port tasks =
-  app.tasks
-
-
 searchFeed : String -> Task x Action
 searchFeed query =
   let
