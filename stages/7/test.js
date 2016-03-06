28434a83b1c70a9da1cc07378e246081b8163c25
Richard Feldman <richard.t.feldman@gmail.com>
2016-03-06 11:14:48 -0800

    Fix test.js across the board

--- stages/7/test.js
+++ stages/7/test.js
@@ -4,7 +4,5 @@ var path = require("path");
 var testDir = path.join(__dirname, "test");
 var binPath = path.join(__dirname, "..", "..", "node_modules", ".bin");
 var elmTestPath = path.join(binPath, "elm-test");
-var elmMakePath = path.join(binPath, "elm-make");
 
-execSync(elmMakePath + " TestRunner.elm --yes --output /dev/null", { cwd: testDir });
 execSync(elmTestPath + " TestRunner.elm", { cwd: testDir, stdio: "inherit" });
