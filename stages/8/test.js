93780caa2cb38bbb4e18dddb0dd8f1eff5495dad
Richard Feldman <richard.t.feldman@gmail.com>
2016-03-24 22:05:27 -0700

    Clear out a bunch of stuff we aren't using anymore.

--- stages/8/test.js
+++
@@ -1,8 +0,0 @@
-var fs = require("fs");
-var execSync = require("child_process").execSync;
-var path = require("path");
-var testDir = path.join(__dirname, "test");
-var binPath = path.join(__dirname, "..", "..", "node_modules", ".bin");
-var elmTestPath = path.join(binPath, "elm-test");
-
-execSync(elmTestPath + " TestRunner.elm", { cwd: testDir, stdio: "inherit" });
