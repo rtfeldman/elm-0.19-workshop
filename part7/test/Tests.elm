1eacda4fa4cbf4e265941ad74fde9b6386d247ef
Richard Feldman <richard.t.feldman@gmail.com>
2016-04-03 08:11:52 -0700

    Add a third failing test to elm-test section.

--- part7/test/Tests.elm
+++ part7/test/Tests.elm
@@ -15,8 +15,8 @@ all =
               """{ "items": [] }"""
            in
             assertEqual
-              (decodeString responseDecoder emptyResponse)
               ({- TODO: what goes here? -})
+              (decodeString responseDecoder emptyResponse)
     , test "they can decode responses with results in them"
         <| let
             response =
@@ -25,10 +25,25 @@ all =
               ] }"""
            in
             assertEqual
-              (decodeString responseDecoder response)
               (Ok
                 [ { id = 5, name = "foo", stars = 42 }
                 , { id = 3, name = "bar", stars = 77 }
                 ]
               )
+              (decodeString responseDecoder response)
+    , test "they result in an error for invalid JSON"
+        <| let
+            response =
+              """{ "pizza": [] }"""
+
+            isErrorResult result =
+              -- TODO return True if the given Result is an Err of some sort,
+              -- and False if it is an Ok of some sort.
+              --
+              -- Result docs: http://package.elm-lang.org/packages/elm-lang/core/3.0.0/Result
+              False
+           in
+            assertEqual
+              True
+              (isError (decodeString responseDecoder response))
     ]
