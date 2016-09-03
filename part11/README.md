dcfb00620b14eea8ac6ca81f22fe6d21473ebe68
Richard Feldman <richard.t.feldman@gmail.com>
2016-09-02 22:27:19 -0700

    Add part11

---
+++ part11/README.md
@@ -0,0 +1,45 @@
+Part 10
+=======
+
+The instructor will paste notes from the lesson, including code examples from
+Q&A, in [this document](https://docs.google.com/document/d/1ApuSOk9DP0YsQrxhW7-WE8UOEAV4PPnLDDeqUOL2o5k/edit?usp=sharing).
+
+## Installation
+
+```bash
+elm-package install
+```
+
+(Answer `y` when prompted.)
+
+
+## Building
+
+```bash
+elm-live Main.elm --open --pushstate --output=elm.js
+```
+
+## Running Tests
+
+Do either (or both!) of the following:
+
+#### Running tests on the command line
+
+```bash
+elm-test
+```
+
+#### Running tests in a browser
+
+```bash
+cd tests
+elm-reactor
+```
+
+Then visit [localhost:8000](http://localhost:8000) and choose `HtmlRunner.elm`.
+
+## References
+
+* [Using Elm packages](https://github.com/elm-lang/elm-package/blob/master/README.md#basic-usage)
+* [elm-test documentation](http://package.elm-lang.org/packages/project-fuzzball/test/latest)
+* [`(<|)` documentation](http://package.elm-lang.org/packages/elm-lang/core/4.0.0/Basics#<|)
---
+++ part11/tests/Tests.elm
@@ -0,0 +1,64 @@
+module Tests exposing (..)
+
+import Test exposing (..)
+import Fuzz exposing (..)
+import Expect exposing (Expectation)
+import ElmHub exposing (responseDecoder)
+import Json.Decode exposing (decodeString, Value)
+import String
+
+
+all : Test
+all =
+    describe "GitHub Response Decoder"
+        [ test "it results in an Err for invalid JSON" <|
+            \() ->
+                let
+                    json =
+                        """{ "pizza": [] }"""
+
+                    isErrorResult result =
+                        case result of
+                            Err _ ->
+                                True
+
+                            Ok _ ->
+                                False
+                in
+                    json
+                        |> decodeString responseDecoder
+                        |> isErrorResult
+                        |> Expect.true "Expected decoding an invalid response to return an Err."
+        , test "it successfully decodes a valid response" <|
+            \() ->
+                """{ "items": [
+                    { "id": 5, "full_name": "foo", "stargazers_count": 42 },
+                    { "id": 3, "full_name": "bar", "stargazers_count": 77 }
+                 ] }"""
+                    |> decodeString responseDecoder
+                    |> Expect.equal
+                        (Ok
+                            [ { id = 5, name = "foo", stars = 42 }
+                            , { id = 3, name = "bar", stars = 77 }
+                            ]
+                        )
+        , fuzz (list int) "it decodes one SearchResult for each 'item' in the JSON" <|
+            \ids ->
+                let
+                    jsonFromId id =
+                        """{"id": """ ++ toString id ++ """, "full_name": "foo", "stargazers_count": 42}"""
+
+                    jsonItems =
+                        String.join ", " (List.map jsonFromId ids)
+
+                    json =
+                        """{ "items": [""" ++ jsonItems ++ """] }"""
+                in
+                    case decodeString responseDecoder json of
+                        Ok results ->
+                            List.length results
+                                |> Expect.equal (List.length ids)
+
+                        Err err ->
+                            Expect.fail ("JSON decoding failed unexpectedly: " ++ err)
+        ]
