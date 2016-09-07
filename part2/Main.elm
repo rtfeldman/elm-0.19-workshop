46a2fe1647dfa01f468c1dc4ae3649ac3010f347
Richard Feldman <richard.t.feldman@gmail.com>
2016-09-07 04:38:39 -0500

    Revise part2 a bit.

--- part2/Main.elm
+++ part2/Main.elm
@@ -22,7 +22,7 @@ elmHubHeader =
 
 view model =
     div [ class "content" ]
-        [ text "TODO replace this text with the contents of elmHubHeader"
+        [ text "TODO put the contents of elmHubHeader here instead of this text!"
         , ul [ class "results" ]
             [ li []
                 [ span [ class "star-count" ]
