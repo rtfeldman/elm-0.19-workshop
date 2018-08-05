7b12aa570f8742eac975b3325a0436cb598b3401
Richard Feldman <richard.t.feldman@gmail.com>
2018-08-05 05:05:34 -0400

    Update default server thing for 0.19

--- server/src/Main.elm
+++ server/src/Main.elm
@@ -7,6 +7,7 @@ port module Main exposing (..)
    setup, and you should ask the instructor for help!
 -}
 
+import Browser
 import Html exposing (Html, h1, img, section, text)
 import Html.Attributes exposing (alt, src, style)
 import Json.Decode exposing (Value)
@@ -18,38 +19,44 @@ port storeSession : Maybe String -> Cmd msg
 port onSessionChange : (Value -> msg) -> Sub msg
 
 
-main : Program Value () ()
+main : Program () () ()
 main =
-    Html.programWithFlags
-        { init = \_ -> ( (), Cmd.none )
-        , update = \() () -> ( (), Cmd.none )
-        , subscriptions = \() -> Sub.none
+    Browser.application
+        { init = \_ _ _ -> ( (), Cmd.none )
+        , onUrlChange = \_ -> ()
+        , onUrlRequest = \_ -> ()
+        , update =
+            \() () ->
+                if False then
+                    ( (), storeSession Nothing )
+
+                else
+                    ( (), Cmd.none )
+        , subscriptions = \() -> onSessionChange (\_ -> ())
         , view =
             \() ->
-                section
-                    [ style
-                        [ ( "margin", "40px auto" )
-                        , ( "width", "960px" )
-                        , ( "text-align", "center" )
+                { title = "Elm 0.19 workshop"
+                , body =
+                    [ section
+                        [ style "margin" "40px auto"
+                        , style "width" "960px"
+                        , style "text-align" "center"
+                        ]
+                        [ h1
+                            [ style "margin" "40px auto"
+                            , style "font-family" "Helvetica, Arial, sans-serif"
+                            , style "font-size" "128px"
+                            , style "color" "rgb(90, 99, 120)"
+                            ]
+                            [ text "You’re all set!" ]
+                        , img
+                            [ alt "Heart in the style of the Elm logo, by Marco Perone"
+                            , src "https://user-images.githubusercontent.com/1094080/39399444-a90f2746-4aeb-11e8-9bd6-4fe45e535921.png"
+                            , style "width" "368px"
+                            , style "height" "305px"
+                            ]
+                            []
                         ]
                     ]
-                    [ h1
-                        [ style
-                            [ ( "margin", "40px auto" )
-                            , ( "font-family", "Helvetica, Arial, sans-serif" )
-                            , ( "font-size", "128px" )
-                            , ( "color", "rgb(90, 99, 120)" )
-                            ]
-                        ]
-                        [ text "You’re all set!" ]
-                    , img
-                        [ alt "Heart in the style of the Elm logo, by Marco Perone"
-                        , src "https://user-images.githubusercontent.com/1094080/39399444-a90f2746-4aeb-11e8-9bd6-4fe45e535921.png"
-                        , style
-                            [ ( "width", "368px" )
-                            , ( "height", "305px" )
-                            ]
-                        ]
-                        []
-                    ]
+                }
         }
