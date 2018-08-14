aed9d1aa5c9b1d0864f13869cf8fbba0d0288f36
Richard Feldman <richard.t.feldman@gmail.com>
2018-08-14 01:46:59 -0400

    Solution to intro/part1

--- intro/part1/Main.elm
+++ intro/part1/Main.elm
@@ -5,25 +5,11 @@ import Html.Attributes exposing (..)
 
 
 banner =
-    {- 👉 TODO: Add a logo and tagline to this banner, so its structure becomes:
-
-          <div class="banner">
-              <div class="container">
-
-                  <h1 class="logo-font">conduit</h1>
-
-                  <p>A place to share your knowledge.</p>
-
-               </div>
-          </div>
-
-       💡 HINT 1: the <div class="row"> above is an element with 2 child nodes.
-
-       💡 HINT 2: the <div class="feed-toggle"> below is an element with text.
-    -}
     div [ class "banner" ]
         [ div [ class "container" ]
-            [ text "👉 TODO: Put the <h1> here instead of this text, then add the <p> right after the <h1>" ]
+            [ h1 [ class "logo-font" ] [ text "conduit" ]
+            , p [] [ text "A place to share your knowledge." ]
+            ]
         ]
 
 
@@ -33,7 +19,7 @@ feed =
 
 main =
     div [ class "home-page" ]
-        [ div [] [ text "👉 TODO: Replace this <div> with the banner" ]
+        [ banner
         , div [ class "container page" ]
             [ div [ class "row" ]
                 [ div [ class "col-md-9" ] [ feed ]
