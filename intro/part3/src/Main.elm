3e6c780a931569a5921128ace51686d9fb175348
Richard Feldman <richard.t.feldman@gmail.com>
2018-08-14 01:55:58 -0400

    Solution to intro/part3

--- intro/part3/src/Main.elm
+++ intro/part3/src/Main.elm
@@ -25,15 +25,11 @@ initialModel =
 
 
 update msg model =
-    {- 👉 TODO: If `msg.description` is "ClickedTag", then
-                set the model's `selectedTag` field to be `msg.data`
+    if msg.description == "ClickedTag" then
+        { model | selectedTag = msg.data }
 
-       💡 HINT: record update syntax looks like this:
-
-                { model | foo = bar }
-
-    -}
-    model
+    else
+        model
 
 
 
@@ -42,16 +38,8 @@ update msg model =
 
 view model =
     let
-        {- 👉 TODO: Filter the articles down to only the ones
-                    that include the currently selected tag.
-
-           💡 HINT: Replace `True` below with something involving
-                    `List.member`, `article.tags`, and `model.selectedTag`
-
-                    Docs for List.member: http://package.elm-lang.org/packages/elm-lang/core/latest/List#member
-        -}
         articles =
-            List.filter (\article -> True)
+            List.filter (\article -> List.member model.selectedTag article.tags)
                 model.allArticles
 
         feed =
@@ -101,17 +89,7 @@ viewTag selectedTagName tagName =
     in
     button
         [ class ("tag-pill " ++ otherClass)
-
-        {- 👉 TODO: Add an `onClick` handler which sends a msg
-                    that our `update` function above will use
-                    to set the currently selected tag to `tagName`.
-
-           💡 HINT: It should look something like this:
-
-                    , onClick { description = … , data = … }
-
-                    👆 Don't forget to add a comma before `onClick`!
-        -}
+        , onClick { description = "ClickedTag", data = tagName }
         ]
         [ text tagName ]
 
